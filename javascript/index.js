c7e881bb50ec76e7337fba4b82617907a7211380
Adam Lipert <34799600+Ortieez@users.noreply.github.com>
2022-03-09 18:34:31 +0100

    Fetch data from a database

--- javascript/index.js
+++ javascript/index.js
@@ -1,47 +1,62 @@
-// Need to add LOGIN (somehow know that account corresponds to the bike...)
-// If time add "service mode that will show some more information"
-
 window.onload = async () => {
-    await fetch("../dump.JSON")
-        .then(res => res.json())
-        .then(data => {
-            // Data will change according to the sent JSON file
-            lat = data["location"]["lat"]
-            long = data["location"]["long"]
-            battery = data["state"]["battery"]
+    var m = new Date();
+    const options = { timeZone: 'Europe/Prague', timeZoneName: 'short' }
+    var dateString =
+        m.getUTCFullYear() + "-" +
+        ("0" + (m.getUTCMonth() + 1)).slice(-2) + "-" +
+        ("0" + m.getUTCDate()).slice(-2) + " " +
+        m.toLocaleTimeString("en-GB", options).slice(0, -4)
 
-            // Here will data be displayed in the index.html
-            span = document.getElementById('batteryPerc')
-            link = `"https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route=${lat}%2C${long}%3B${lat}%2C${long}"`
+    var strings = dateString + "|ZIDAN|Ad0oNkQWJi7LJBsQoCqV"
+    var pbHash = SHA1(strings)
 
-            document.getElementById("getMap").setAttribute('onclick', 'location.href=' + link + ';')
-            span.innerHTML = battery + "%"
-            var map = L.map("map", { zoomControl: false }).setView(
-                [lat, long],
-                20
-            );
-            L.tileLayer(
-                "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=lIVs7d71OHlrFzcb54Lf",
-                {
-                    maxZoom: 30,
-                }
-            ).addTo(map);
-            var circle = L.circle([lat, long], {
-                color: '#008FCC',
-                fillColor: '#00C0EA',
-                fillOpacity: 0.5,
-                radius: 2.5
-            }).addTo(map);
-            var circle = L.circle([lat, long], {
-                color: '#008FCC',
-                fillColor: '#008FCC',
-                fillOpacity: 1,
-                radius: 0.7
-            }).addTo(map);
+    var config =
+    {
+        dttm: dateString,
+        vehicle_code: "ZIDAN",
+        signature: pbHash
+    }
+    const response = await fetch("http://emoto-api.bubileg.cz/get-last-log", {
+        method: "POST",
+        body: JSON.stringify(config),
+    }).then(res => res.json())
 
-            batteryColor = document.getElementById('batteryColor')
-            batteryColor.style.background = getColor(battery / 100)
-        })
+    gps = response["log"]["gps"]
+    gps = gps.split(", ")
+    gps[0] = gps[0].slice(0, -2)
+    gps[1] = gps[1].slice(0, -2)
+    battery = response["log"]["battery_capacity"]
+
+    span = document.getElementById('batteryPerc')
+    link = `"http://www.google.com/maps/place/${gps[0]},${gps[1]}/data=!3m1!4b1"`
+    document.getElementById("getMap").setAttribute('onclick', 'location.href=' + link + ';')
+    span.innerHTML = battery + "%"
+
+    var map = L.map("map", { zoomControl: false }).setView(
+        [gps[0], gps[1]],
+        20
+    );
+    L.tileLayer(
+        "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=lIVs7d71OHlrFzcb54Lf",
+        {
+            maxZoom: 30,
+        }
+    ).addTo(map);
+    var circle = L.circle([gps[0], gps[1]], {
+        color: '#008FCC',
+        fillColor: '#00C0EA',
+        fillOpacity: 0.5,
+        radius: 2.5
+    }).addTo(map);
+    var circle = L.circle([gps[0], gps[1]], {
+        color: '#008FCC',
+        fillColor: '#008FCC',
+        fillOpacity: 1,
+        radius: 0.7
+    }).addTo(map);
+
+    batteryColor = document.getElementById('batteryColor')
+    batteryColor.style.background = getColor(battery / 100)
 }
 
 function getColor(value) {
@@ -53,6 +68,7 @@ function getColor(value) {
 var lever = false;
 function slide() {
     var top = document.getElementById("top");
+    var topText = document.getElementById('topText');
     var tl = anime.timeline({
         duration: 500,
         easing: "easeInOutQuad"
@@ -66,7 +82,14 @@ function slide() {
             .add({
                 targets: "#arrow",
                 rotate: "180deg",
-            }, "-=500");
+            }, "-=500")
+            .add({
+                targets: topText,
+                opacity: 1,
+                begin: function () {
+                    document.getElementById('topText').style.display = 'flex';
+                },
+            }, "-=200");
         lever = true;
     } else {
         tl.add({
@@ -76,7 +99,148 @@ function slide() {
             .add({
                 targets: "#arrow",
                 rotate: "0deg",
-            }, "-=500");
+            }, "-=500")
+            .add({
+                targets: topText,
+                opacity: 0,
+                begin: function () {
+                    document.getElementById('topText').style.display = 'none';
+                },
+            }, "-=100");
         lever = false;
     }
+}
+
+function SHA1(msg) {
+    function rotate_left(n, s) {
+        var t4 = (n << s) | (n >>> (32 - s));
+        return t4;
+    };
+    function lsb_hex(val) {
+        var str = '';
+        var i;
+        var vh;
+        var vl;
+        for (i = 0; i <= 6; i += 2) {
+            vh = (val >>> (i * 4 + 4)) & 0x0f;
+            vl = (val >>> (i * 4)) & 0x0f;
+            str += vh.toString(16) + vl.toString(16);
+        }
+        return str;
+    };
+    function cvt_hex(val) {
+        var str = '';
+        var i;
+        var v;
+        for (i = 7; i >= 0; i--) {
+            v = (val >>> (i * 4)) & 0x0f;
+            str += v.toString(16);
+        }
+        return str;
+    };
+    function Utf8Encode(string) {
+        string = string.replace(/\r\n/g, '\n');
+        var utftext = '';
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if ((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        return utftext;
+    };
+    var blockstart;
+    var i, j;
+    var W = new Array(80);
+    var H0 = 0x67452301;
+    var H1 = 0xEFCDAB89;
+    var H2 = 0x98BADCFE;
+    var H3 = 0x10325476;
+    var H4 = 0xC3D2E1F0;
+    var A, B, C, D, E;
+    var temp;
+    msg = Utf8Encode(msg);
+    var msg_len = msg.length;
+    var word_array = new Array();
+    for (i = 0; i < msg_len - 3; i += 4) {
+        j = msg.charCodeAt(i) << 24 | msg.charCodeAt(i + 1) << 16 |
+            msg.charCodeAt(i + 2) << 8 | msg.charCodeAt(i + 3);
+        word_array.push(j);
+    }
+    switch (msg_len % 4) {
+        case 0:
+            i = 0x080000000;
+            break;
+        case 1:
+            i = msg.charCodeAt(msg_len - 1) << 24 | 0x0800000;
+            break;
+        case 2:
+            i = msg.charCodeAt(msg_len - 2) << 24 | msg.charCodeAt(msg_len - 1) << 16 | 0x08000;
+            break;
+        case 3:
+            i = msg.charCodeAt(msg_len - 3) << 24 | msg.charCodeAt(msg_len - 2) << 16 | msg.charCodeAt(msg_len - 1) << 8 | 0x80;
+            break;
+    }
+    word_array.push(i);
+    while ((word_array.length % 16) != 14) word_array.push(0);
+    word_array.push(msg_len >>> 29);
+    word_array.push((msg_len << 3) & 0x0ffffffff);
+    for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
+        for (i = 0; i < 16; i++) W[i] = word_array[blockstart + i];
+        for (i = 16; i <= 79; i++) W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+        A = H0;
+        B = H1;
+        C = H2;
+        D = H3;
+        E = H4;
+        for (i = 0; i <= 19; i++) {
+            temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+        for (i = 20; i <= 39; i++) {
+            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+        for (i = 40; i <= 59; i++) {
+            temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+        for (i = 60; i <= 79; i++) {
+            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+        H0 = (H0 + A) & 0x0ffffffff;
+        H1 = (H1 + B) & 0x0ffffffff;
+        H2 = (H2 + C) & 0x0ffffffff;
+        H3 = (H3 + D) & 0x0ffffffff;
+        H4 = (H4 + E) & 0x0ffffffff;
+    }
+    var temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+
+    return temp.toLowerCase();
 }
